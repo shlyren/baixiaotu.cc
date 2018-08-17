0c67f9e85dd71fde1f29afbbbefe82961db0e086
RenYuXiang <mail@yuxiang.ren>
2018-08-17 11:53:46 +0800

    update

--- Sources/baixiaotu/Config.swift
+++ Sources/baixiaotu/Config.swift
@@ -10,68 +10,17 @@ import Foundation
 public struct Config {
     
     public struct MYSQL {
+        public static var host = ""
+        public static var user = ""
+        public static var password = ""
         
-        public static var host: String {
-            get {
-                return SQLConfig["host"] ?? ""
-            }
-            set {
-                SQLConfig["host"] = newValue
-            }
-        }
-        public static var user: String {
-            get {
-                return SQLConfig["user"] ?? ""
-            }
-            set {
-                SQLConfig["user"] = newValue
-            }
-            
-        }
-        public static var password: String {
-            get {
-                return SQLConfig["password"] ?? ""
-            }
-            set {
-                SQLConfig["password"] = newValue
-            }
-            
-        }
     }
     
     public struct SMTP {
-        
-        public static var url: String {
-            get {
-                return SMTPConfig["url"] ?? ""
-            }
-            set {
-                SMTPConfig["url"] = newValue
-            }
-        }
-        public static var user: String {
-            get {
-                return SMTPConfig["user"] ?? ""
-            }
-            set {
-                SMTPConfig["user"] = newValue
-            }
-            
-        }
-        public static var password: String {
-            get {
-                return SMTPConfig["password"] ?? ""
-            }
-            set {
-                SMTPConfig["password"] = newValue
-            }
-            
-        }
+        public static var url = ""
+        public static var user = ""
+        public static var password = ""
     }
-    
-    
-    private static var SQLConfig = [String: String]()
-    private static var SMTPConfig = [String: String]()
     
     /**
      配置信息, configPath 为配置文件路径, json格式为:
@@ -95,16 +44,18 @@ public struct Config {
         guard let data = try? Data.init(contentsOf: url, options: Data.ReadingOptions.mappedIfSafe) else {
             return
         }
-        guard let obj = try? JSONSerialization.jsonObject(with: data, options: []) else {
-            return
-        }
-
-        guard let config = obj as? [String: [String: String]]  else {
-            return
-        }
-
-        SQLConfig = config["mysql"]!
-        SMTPConfig = config["smtp"]!
+        let obj = try? JSONSerialization.jsonObject(with: data, options: [])
+        let config = obj as? [String: [String: String]]
+        
+        let sql = config?["mysql"]
+        MYSQL.host = sql?["host"] ?? ""
+        MYSQL.user = sql?["user"] ?? ""
+        MYSQL.password = sql?["password"] ?? ""
+        
+        let smtp = config?["smtp"]
+        SMTP.url = smtp?["url"] ?? ""
+        SMTP.user = smtp?["user"] ?? ""
+        SMTP.password = smtp?["password"] ?? ""
     }
     
     
