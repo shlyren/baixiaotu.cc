288c914bcde7e1769f84dfb9b57fabdcf2d2e463
RenYuXiang <mail@yuxiang.ren>
2018-09-17 18:05:31 +0800

    update

--- Sources/html/js/gitment.js
+++ Sources/html/js/gitment.js
@@ -3414,7 +3414,7 @@ var Gitment = function () {
       }, options);
 
       this.state.user.isLoggingIn = true;
-      _utils.http.post('https://gh-oauth.imsun.net', {
+      _utils.http.post('https://auth.baixiaotu.cc', {
         code: code,
         client_id: client_id,
         client_secret: client_secret
