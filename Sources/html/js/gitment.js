2a0e5aa40ed382869eea659e0e1d6143d13290b0
RenYuXiang <mail@yuxiang.ren>
2018-09-20 08:30:28 +0800

    update

--- Sources/html/js/gitment.js
+++ Sources/html/js/gitment.js
@@ -3414,7 +3414,7 @@ var Gitment = function () {
       }, options);
 
       this.state.user.isLoggingIn = true;
-      _utils.http.post('https://auth.baixiaotu.cc', {
+      _utils.http.post('https://github.com/login/oauth/access_token', {
         code: code,
         client_id: client_id,
         client_secret: client_secret
